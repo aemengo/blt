dbb05ec485802eab612190d80e1e360984ee41e4
Anthony Emengo <anthonyemengojr@gmail.com>
2018-09-09 23:23:37 -0400

    wip

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/aemengo/bosh-runc-cpi/pb"
+	"github.com/jessevdk/go-flags"
+	"google.golang.org/grpc"
+	"os"
+)
+
+
+var opts struct {
+	Target string `short:"t" long:"target" description:"Target the tcp address of a runc cpid server in the following format" value-name:"127.0.0.1:9999"`
+}
 
 func main()  {
-	fmt.Println("hello world!")
+	args, err := flags.Parse(&opts)
+	expectNoError(err)
+
+	conn, err := grpc.Dial(opts.Target, grpc.WithInsecure())
+	expectNoError(err)
+	defer conn.Close()
+
+	ctx := context.Background()
+	cpidClient := pb.NewCPIDClient(conn)
+
+}
+
+func expectNoError(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
 }
